db7f32d5e7d0722ecff6bdd97af1c7d841515522
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-11-05 15:54:18 +0530

    Add files via upload

---
+++ Week1/day1/MyFirstProgram.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class MyFirstProgram {
+	// explain the signature by which the main method is implemented ?
+	public static  void main(String[] args) {  //content assist --> ctrl + space (win)
+		System.out.println("Welcome to Java World");
+	// ctrl + F11(fn+f11) KEYS - execute a program	
+}} 
+
+
